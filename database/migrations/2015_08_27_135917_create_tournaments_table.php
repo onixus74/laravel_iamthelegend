df0505cbb9bb4029c3aedc01cf32ef1bd6f73ba8
onixus74 <marwen.ounis@esprit.tn>
2015-08-27 18:51:46 +0100

    Create Tournament

---
+++ database/migrations/2015_08_27_135917_create_tournaments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTournamentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tournaments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer("teams_number");
+            $table->dateTime('start_time');
+            $table->timestamps();
+        });
+
+        //Pivot Table for tournament team
+        Schema::create('tournament_team', function (Blueprint $table) {
+            $table->integer('tournament_id')->unsigned()->index();
+            $table->foreign('tournament_id')->references('id')->on('tournaments')->onDelete('cascade');
+
+            $table->integer('team_id')->unsigned()->index();
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+       Schema::drop('tournaments') ;
+        Schema::drop('tournament_team') ;
+    }
+}
